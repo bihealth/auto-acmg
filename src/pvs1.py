06306581790543a0354c1c8bae14d348612cd096
gromdimon <grom.dima.grom@gmail.com>
2024-03-13 18:48:06 +0100

    nmd rule

--- src/pvs1.py
+++ src/pvs1.py
@@ -1,11 +1,13 @@
 """Implementations of the PVS1 algorithm."""
 
+import asyncio
 import logging
 import random
+import re
 import string
 
 from src.api.mehari import MehariClient
-from src.seqvar import SeqVar
+from src.seqvar import SeqVar, SeqVarResolver
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -16,6 +18,8 @@ class PVS1:
 
     def __init__(self, seqvar: SeqVar):
         self.seqvar = seqvar
+        self.pHGVS: list[str] = []
+        self.tHGVS: list[str] = []
         self.seqvar_transcripts = None
         self.gene_transcripts = None
         self.gene_hgnc_id = None
@@ -53,9 +57,17 @@ class PVS1:
             if self.seqvar_transcripts and len(self.seqvar_transcripts) > 0:
                 self.gene_hgnc_id = self.seqvar_transcripts[0]["gene_id"]
 
+                # Reset pHGVS and tHGVS lists
+                self.pHGVS = []
+                self.tHGVS = []
+                for transcript in self.seqvar_transcripts:
+                    self.pHGVS.append(transcript["feature_id"] + ":" + transcript["hgvs_p"])
+                    self.tHGVS.append(transcript["feature_id"] + ":" + transcript["hgvs_t"])
+
                 result = await mehari_client.get_gene_transcripts(
                     self.gene_hgnc_id, self.seqvar.genome_release
                 )
+
                 if result["transcripts"]:
                     self.gene_transcripts = result["transcripts"]
                 else:
@@ -66,6 +78,40 @@ class PVS1:
                 f"Failed to get transcripts for variant {self.seqvar.user_representation}."
             )
             logger.error(e)
+
+    @staticmethod
+    def _get_pHGVS_termination(pHGVS):
+        """
+        Get termination position from pHGVS:
+        # NM_031475.2:p.Gln98*
+        # NM_031475.2:p.Ala586Glyfs*73
+        # NP_000305.3:p.Arg378SerfsTer5
+
+        # p.Arg97Glyfs*26 (alternatively p.Arg97GlyfsTer26, or short p.Arg97fs)
+        """
+        if "fs" in pHGVS:
+            pattern1 = re.compile(r"p\.\D+(\d+)\D+fs(\*|X|Ter)(\d+)")
+            match1 = pattern1.search(pHGVS)
+            pattern2 = re.compile(r"p\.\D+(\d+)fs")
+            match2 = pattern2.search(pHGVS)
+
+            if match1:
+                # if int(match1.group(1)) / (self.transcript.cds_length/3) > 0.5:
+                termination = int(match1.group(1)) + int(match1.group(3))
+                # else:
+                #    termination = int((self.transcript.cds_length/3)/2)
+            elif match2:
+                termination = int(match2.group(1))
+            else:
+                termination = -1
+
+        elif "*" in pHGVS or "X" in pHGVS or "Ter" in pHGVS:
+            pattern = re.compile(r"p\.\D+(\d+)(\*|X|Ter)")
+            match = pattern.search(pHGVS)
+            termination = int(match.group(1)) if match else -1
+        else:
+            termination = -1
+        return termination
 
     def _is_nmd_target(self) -> bool:
         """
@@ -78,10 +124,28 @@ class PVS1:
         :return: NMD prediction
         :rtype: bool
         """
+        assert self.gene_transcripts is not None
+        for pHGVS in self.pHGVS:
+            new_stop_codon = self._get_pHGVS_termination(pHGVS)
+            cds_sizes = [
+                exon["altEndI"] - exon["altStartI"]
+                for exon in self.gene_transcripts[0]["genomeAlignments"][0]["exons"]
+            ]
+            if self.gene_hgnc_id == "HGNC:4284":  # Hearing Loss Guidelines GJB2
+                return True
+            elif len(cds_sizes) <= 1:
+                continue
+            else:
+                nmd_cutoff = sum(cds_sizes[:-1]) - min(50, cds_sizes[-2])
+                if new_stop_codon * 3 <= nmd_cutoff:
+                    return True
+                else:
+                    continue
         return False
 
     async def run(self):
         """Make the PVS1 prediction."""
         await self._get_transcripts()
-
+        if self.seqvar_transcripts and self.gene_transcripts:
+            self._is_nmd_target()
         # self.vep
