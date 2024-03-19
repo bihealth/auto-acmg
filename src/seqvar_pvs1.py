a9b5a1fd8c95a093de05a22cb6b829c84c8a3e1a
gromdimon <grom.dima.grom@gmail.com>
2024-03-19 18:19:06 +0100

    lof removes 10 percent of protein :

--- src/seqvar_pvs1.py
+++ src/seqvar_pvs1.py
@@ -3,7 +3,7 @@
 import logging
 import re
 from enum import Enum, auto
-from typing import Dict
+from typing import Dict, List
 
 from src.core.config import settings
 from src.seqvar import SeqVar
@@ -38,10 +38,10 @@ class SeqVarPVS1:
 
         # Frameshift/Nonsense attributes
         self.undergo_nmd: bool = False
-        self.in_relevant_transcript: bool = False
-        self.critical4protein: bool = False
-        self.is_frequent_in_population: bool = False
-        self.remove_much_of_protein: bool = False
+        self.is_biologically_relevant: bool = False
+        self.is_critical_for_protein_function: bool = False
+        self.LoF_exon_is_frequent_in_pop: bool = False
+        self.LoF_removes_10_percent_of_protein: bool = False
 
     def _initialize(self):
         """Setup the PVS1 class."""
@@ -49,28 +49,41 @@ class SeqVarPVS1:
         self.pHGVS = self.HGVS + ":" + self.seqvar_transcripts["hgvs_p"]
         self.tHGVS = self.HGVS + ":" + self.seqvar_transcripts["hgvs_t"]
         self.gene_hgnc_id = self.seqvar_transcripts["gene_id"]
+        self.transcript_tags = self.seqvar_transcripts["feature_tag"]
+        self.cds_sizes = [
+            exon["altEndI"] - exon["altStartI"]
+            for exon in self.gene_transcripts["genomeAlignments"][0]["exons"]
+        ]
+        self.cds_length = sum(self.cds_sizes)
 
     def verify_PVS1(self):
         """Make the PVS1 prediction."""
         if self.consequence == SeqVarConsequence.NonsenseFrameshift:
-            self.undergo_nmd = self._undergo_nmd()
-            if self.undergo_nmd:
-                self.in_relevant_transcript = self._in_biologically_relevant_transcript()
-                if self.in_relevant_transcript:
+            # Follow guidelines for PTEN
+            if self.gene_hgnc_id == "PTEN":
+                if self._get_pHGVS_termination(self.pHGVS) < 374:
+                    self.prediction = PVS1Prediction.PVS1
+                    return
+
+            if self._undergo_nmd():
+                if self._in_biologically_relevant_transcript(self.transcript_tags):
                     self.prediction = PVS1Prediction.PVS1
                 else:
                     self.prediction = PVS1Prediction.NotPVS1
             else:
-                self.critical4protein = self._critical4protein_function()
-                if self.critical4protein:
+                if self._critical4protein_function():
                     self.prediction = PVS1Prediction.PVS1_Strong
                 else:
-                    self.is_frequent_in_population = self._lof_is_frequent_in_population()
-                    if self.is_frequent_in_population:
+                    # Guidelines are unclear on "and/or" statement here.
+                    if (
+                        self._lof_is_frequent_in_population()
+                        or not self._in_biologically_relevant_transcript(self.transcript_tags)
+                    ):
                         self.prediction = PVS1Prediction.NotPVS1
                     else:
-                        self.remove_much_of_protein = self._remove_more_then_10_percent_of_protein()
-                        if self.remove_much_of_protein:
+                        if self._lof_removes_more_then_10_percent_of_protein(
+                            self.pHGVS, self.cds_length
+                        ):
                             self.prediction = PVS1Prediction.PVS1_Strong
                         else:
                             self.prediction = PVS1Prediction.PVS1_Moderate
@@ -128,27 +141,22 @@ class SeqVarPVS1:
         See more at https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6185798/#:~:text=Generally%2C%20NMD%20is%20not%20predicted%20to%20occur%20if%20the%20premature%20termination%20codon%20occurs%20in%20the%203%E2%80%99%20most%20exon%20or%20within%20the%203%E2%80%99%2Dmost%2050%20nucleotides%20of%20the%20penultimate%20exon
         """
         # Ensure that necessary data is available
-        assert self.gene_transcripts is not None
+        assert self.cds_sizes is not None
 
         new_stop_codon = self._get_pHGVS_termination(self.pHGVS)
-        cds_sizes = [
-            exon["altEndI"] - exon["altStartI"]
-            for exon in self.gene_transcripts["genomeAlignments"][0]["exons"]
-        ]
         if self.gene_hgnc_id == "HGNC:4284":  # Hearing Loss Guidelines GJB2
             return True
-        elif len(cds_sizes) <= 1:
+        elif len(self.cds_sizes) <= 1:
             return False
         else:
-            nmd_cutoff = sum(cds_sizes[:-1]) - min(50, cds_sizes[-2])
+            nmd_cutoff = sum(self.cds_sizes[:-1]) - min(50, self.cds_sizes[-2])
             return new_stop_codon * 3 <= nmd_cutoff
 
-    def _in_biologically_relevant_transcript(self) -> bool:
+    @staticmethod
+    def _in_biologically_relevant_transcript(transcript_tags: List[str]) -> bool:
         """Check if the exon with SeqVar is in a biologically relevant transcript."""
         # Ensure that necessary data is available
-        assert self.seqvar_transcripts is not None
-
-        return "ManeSelect" in self.seqvar_transcripts["feature_tag"]
+        return "ManeSelect" in transcript_tags
 
     def _critical4protein_function(self) -> bool:
         """Check if the truncated/altered region is critical for the protein function."""
@@ -157,8 +165,17 @@ class SeqVarPVS1:
 
     def _lof_is_frequent_in_population(self) -> bool:
         """Check if the LoF variants in the exon are frequent in the general population."""
+        # TODO: Implemnt this method
         return False
 
-    def _remove_more_then_10_percent_of_protein(self) -> bool:
-        """Check if the protein is 10% of the normal length."""
-        return False
+    @staticmethod
+    def _lof_removes_more_then_10_percent_of_protein(pHGVS: str, cds_length: int) -> bool:
+        """Check if the LoF variant removes more than 10% of the protein."""
+        pattern = re.compile(r"p\.\D+(\d+)(\D+fs)?(\*|X|Ter)(\d+)?")
+        match = pattern.search(pHGVS)
+        codon_offset = int(match.group(1)) if match else -1
+        codon_length = cds_length / 3
+        if codon_offset > 0 and (codon_length - codon_offset) / codon_length > 0.1:
+            return True
+        else:
+            return False
