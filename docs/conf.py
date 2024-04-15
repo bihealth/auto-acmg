a63152d550dbae3a3fc6dfebf17e1c096d54645d
Dzmitry Hramyka <grom.dima.grom@gmail.com>
2024-04-15 10:35:58 +0200

    Fix sphinx docs in production + support awesome theme (#39)
    * sphinx new theme
    
    * strucvar docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,5 +30,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
-html_static_path = ["_static"]
+html_permalinks_icon = "<span>#</span>"
+html_theme = "sphinxawesome_theme"
