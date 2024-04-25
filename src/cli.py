582e687d2e3459454862b9debe33427f04c0647e
gromdimon <grom.dima.grom@gmail.com>
2024-04-25 18:42:50 +0200

    restructure for acmg

--- src/cli.py
+++ src/cli.py
@@ -3,9 +3,9 @@
 import typer
 from typing_extensions import Annotated
 
-from src.pvs1.auto_pvs1 import AutoPVS1
 from src.auto_acmg import AutoACMG
 from src.defs.genome_builds import GenomeRelease
+from src.pvs1.auto_pvs1 import AutoPVS1
 
 app = typer.Typer()
 
@@ -15,46 +15,6 @@ ALLOWED_GENOME_RELEASES = ["GRCh37", "GRCh38", "hg19", "hg38", "grch37", "grch38
 ALLOWED_SEQVAR_FORMATS = ["Canonical SPDI", "gnomAD", "relaxed SPDI", "dbSNP", "ClinVar"]
 #: Allowed structural variant formats
 ALLOWED_STRUCVAR_FORMATS = ["Colon-separated", "Hyphen-separated"]
-
-
-@app.command()
-def predict_pvs1(
-    variant: Annotated[
-        str,
-        typer.Argument(
-            help=(
-                f"Variant to be classified, e.g., 'NM_000038.3:c.797G>A'. "
-                f"Accepted sequence variants formats: {', '.join(ALLOWED_SEQVAR_FORMATS)}. "
-                f"Accepted structural variants formats: {', '.join(ALLOWED_STRUCVAR_FORMATS)}."
-            )
-        ),
-    ],
-    genome_release: Annotated[
-        str,
-        typer.Option(
-            "--genome-release",
-            "-g",
-            help=f"Accepted genome Releases: {', '.join(ALLOWED_GENOME_RELEASES)}",
-        ),
-    ] = "GRCh38",
-):
-    """
-    Predict the PVS1 criteria for a given variant using the specified genome release.
-    """
-    try:
-        genome_release_enum = GenomeRelease.from_string(genome_release)
-        if not genome_release_enum:
-            raise ValueError(
-                (
-                    f"Invalid genome release: {genome_release}. "
-                    f"Please use one of {', '.join(ALLOWED_GENOME_RELEASES)}."
-                )
-            )
-
-        auto_pvs1 = AutoPVS1(variant, genome_release_enum)
-        auto_pvs1.predict()
-    except Exception as e:
-        typer.secho(f"Error: {e}", err=True, fg=typer.colors.RED)
 
 
 @app.command()
