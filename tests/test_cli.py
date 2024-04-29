fc37bd805c8b5ce8c26b3ef1bde19114b8da128d
Dzmitry Hramyka <grom.dima.grom@gmail.com>
2024-04-29 16:44:56 +0200

    Restructure module in order to support other acmg rules (#83)
    * wip
    
    * restructure for acmg
    
    * restructure dependencies for dev installation
    
    * restructure different files

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,30 +10,30 @@ runner = CliRunner()
 
 
 @pytest.fixture
-def mock_auto_pvs1_predict_success():
-    """Fixture to mock AutoPVS1 predict method with a success response."""
-    with patch("src.autoPVS1.AutoPVS1.predict") as mock_predict:
+def mock_auto_acmg_predict_success():
+    """Fixture to mock AutoACMG predict method with a success response."""
+    with patch("src.auto_acmg.AutoACMG.predict") as mock_predict:
         mock_predict.return_value = "Pathogenic"
         yield mock_predict
 
 
 @pytest.fixture
-def mock_auto_pvs1_predict_failure():
+def mock_auto_acmg_predict_failure():
     """Fixture to mock AutoPVS1 predict method with a failure response."""
-    with patch("src.autoPVS1.AutoPVS1.predict") as mock_predict:
+    with patch("src.auto_acmg.AutoACMG.predict") as mock_predict:
         mock_predict.side_effect = Exception("An error occurred")
         yield mock_predict
 
 
-def test_classify_command_success(mock_auto_pvs1_predict_success):
-    """Test the 'classify' command with a mocked success response from AutoPVS1.predict."""
+def test_classify_command_success(mock_auto_acmg_predict_success):
+    """Test the 'classify' command with a mocked success response from AutoACMG.predict."""
     result = runner.invoke(app, ["NM_000038.3:c.797G>A", "--genome-release", "GRCh38"])
     assert result.exit_code == 0
     assert "" in result.stdout
 
 
-def test_classify_command_failure(mock_auto_pvs1_predict_failure):
-    """Test the 'classify' command with a mocked failure response from AutoPVS1.predict."""
+def test_classify_command_failure(mock_auto_acmg_predict_failure):
+    """Test the 'classify' command with a mocked failure response from AutoACMG.predict."""
     result = runner.invoke(app, ["NM_000038.3:c.797G>A", "--genome-release", "GRCh38"])
     assert result.exit_code == 0
     assert "Error: An error occurred" in result.stdout
