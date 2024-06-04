ef8062b4ef568b9bf81501987b320521c785aedb
Dzmitry Hramyka <grom.dima.grom@gmail.com>
2024-06-04 09:40:06 +0200

    Implement SpliceAI scores check and proximity to the splice site check for the `AutoBP7` (#125)
    * Implement SpliceAI scores check and proximity to the splice site
    
    * cleanup
    
    * wip
    
    * fix int -> float
    
    * Add more e2e tests

--- docs/dev_quickstart.rst
+++ docs/dev_quickstart.rst
@@ -60,7 +60,7 @@ Clone Rsepository
 .. code-block:: bash
 
     git clone git@github.com:bihealth/auto-acmg.git
-  
+
 --------------------
 Install Dependencies
 --------------------
@@ -70,6 +70,39 @@ You can use the provided ``Makefile`` files to install the dependencies.
 .. code-block:: bash
 
     make deps
+
+------------------
+Set up the SeqRepo
+------------------
+
+AutoACMG uses the SeqRepo to fetch the reference sequences.
+To set up the SeqRepo, you'll need to have the RefSeq reference genomes of versions GRCh37 and
+GRCh38. You can get them as follows:
+
+1. Initialize the SeqRepo:
+
+.. code-block:: bash
+
+    seqrepo init -i auto-acmg
+
+.. note::
+
+    The ``-i auto-acmg`` flag is used to set the SeqRepo instance name to ``auto-acmg``.
+    If you want to use a different default seqrepo directory, you can set the ``SEQREPO_DIR``
+    environment variable or provide the ``-r`` flag to the ``seqrepo`` command.
+
+
+2. Download the reference genomes:
+
+.. code-block:: bash
+
+    seqrepo fetch-load -i auto-acmg -n RefSeq NC_000001.10 NC_000002.11 NC_000003.11 NC_000004.11 NC_000005.9 NC_000006.11 NC_000007.13 NC_000008.10 NC_000009.11 NC_000010.10 NC_000011.9 NC_000012.11 NC_000013.10 NC_000014.8 NC_000015.9 NC_000016.9 NC_000017.10 NC_000018.9 NC_000019.9 NC_000020.10 NC_000021.8 NC_000022.10 NC_000023.10 NC_000024.9 NC_012920.1 NC_000001.11 NC_000002.12 NC_000003.12 NC_000004.12 NC_000005.10 NC_000006.12 NC_000007.14 NC_000008.11 NC_000009.12 NC_000010.11 NC_000011.10 NC_000012.12 NC_000013.11 NC_000014.9 NC_000015.10 NC_000016.10 NC_000017.11 NC_000018.10 NC_000019.10 NC_000020.11 NC_000021.9 NC_000022.11 NC_000023.11 NC_000024.10 NC_012920.1
+
+.. note::
+
+    The above RefSeq identifiers are for all of the chromosomes and the mitochondrial genome. Note,
+    that this is a large (and slow) download and will take some time.
+
 
 ----------------------
 Set up the `.env` file
--- src/criteria/auto_bp7.py
+++ src/criteria/auto_bp7.py
@@ -1,6 +1,6 @@
 """Implementation of BP7 criteria."""
 
-from typing import Optional
+from typing import List, Optional
 
 from loguru import logger
 
@@ -10,8 +10,9 @@ from src.defs.annonars_variant import VariantResult
 from src.defs.auto_acmg import BP7
 from src.defs.exceptions import AutoAcmgBaseException, MissingDataError
 from src.defs.genome_builds import GenomeRelease
+from src.defs.mehari import Exon
 from src.defs.seqvar import SeqVar
-from src.utils import SeqVarTranscriptsHelper
+from src.utils import SeqVarTranscriptsHelper, SplicingPrediction
 
 
 class AutoBP7:
@@ -47,7 +48,17 @@ class AutoBP7:
             return None
 
     def _check_proximity_to_pathogenic_variants(self, seqvar: SeqVar) -> bool:
-        """Check for pathogenic variants +/- 2bp of the position in ClinVar."""
+        """
+        Check for pathogenic variants +/- 2bp of the position in ClinVar.
+
+        Check the ClinVar data for variants within 2bp of the position.
+
+        Attributes:
+            seqvar (SeqVar): The variant to check.
+
+        Returns:
+            bool: True if pathogenic variants are found, False otherwise.
+        """
         response = self.annonars_client.get_variant_from_range(
             seqvar, seqvar.pos - 2, seqvar.pos + 2
         )
@@ -66,13 +77,69 @@ class AutoBP7:
         return False
 
     def _check_proximity_to_splice_site(self, seqvar: SeqVar) -> bool:
-        """Check if the variant is closer than 2bp to a splice site."""
-        # TODO: Implement this method.
+        """
+        Check if the variant is closer than 2bp to a splice site.
+
+        Check if the variant is within 2bp from the start or end of an exon.
+
+        Attributes:
+            seqvar (SeqVar): The variant to check.
+
+        Returns:
+            bool: True if the variant is within 2bp of a splice site, False otherwise.
+        """
+        # Fetch transcript data
+        seqvar_transcript_helper = SeqVarTranscriptsHelper(seqvar, config=self.config)
+        seqvar_transcript_helper.initialize()
+        (
+            _,
+            gene_transcript,
+            _,
+            _,
+            _,
+        ) = seqvar_transcript_helper.get_ts_info()
+        if (
+            not gene_transcript
+            or not gene_transcript.genomeAlignments
+            or not gene_transcript.genomeAlignments[0].exons
+        ):
+            logger.warning("No exons found for the transcript.")
+            return False
+
+        # Check if the variant is within 2bp of a start or end of an exon
+        for exon in gene_transcript.genomeAlignments[0].exons:
+            if abs(seqvar.pos - exon.altCdsStartI) <= 2 or abs(exon.altCdsEndI - seqvar.pos) <= 2:
+                return True
         return False
 
     def _predict_spliceai(self, seqvar: SeqVar) -> bool:
-        """Predict splice site alterations using SpliceAI."""
-        # TODO: Implement this method.
+        """
+        Predict splice site alterations using SpliceAI.
+
+        Fetch the SpliceAI data from CADD and predict if the variant is a splice site alteration.
+        If any of SpliceAI scores are greater than 0, the variant is considered a splice site
+        alteration.
+
+        Attributes:
+            seqvar (SeqVar): The variant to check.
+
+        Returns:
+            bool: True if the variant is a splice site alteration, False otherwise.
+        """
+        variant_info = self._get_variant_info(seqvar)
+        if not variant_info or not variant_info.cadd:
+            raise MissingDataError("Missing CADD data for variant.")
+        acc_gain = variant_info.cadd.SpliceAI_acc_gain
+        acc_loss = variant_info.cadd.SpliceAI_acc_loss
+        don_gain = variant_info.cadd.SpliceAI_don_gain
+        don_loss = variant_info.cadd.SpliceAI_don_loss
+        if (
+            (acc_gain and acc_gain > 0)
+            or (acc_loss and acc_loss > 0.5)
+            or (don_gain and don_gain > 0)
+            or (don_loss and don_loss > 0.5)
+        ):
+            return True
         return False
 
     def predict(self) -> Optional[BP7]:
@@ -91,9 +158,9 @@ class AutoBP7:
                 self.prediction.BP7 = False
                 return self.prediction
             if self._predict_spliceai(self.seqvar):
-                self.prediction.BP7 = True
-            else:
                 self.prediction.BP7 = False
+            else:
+                self.prediction.BP7 = True
         except AutoAcmgBaseException as e:
             logger.error("Failed to predict BP7 criterion. Error: {}", e)
             self.prediction = None
