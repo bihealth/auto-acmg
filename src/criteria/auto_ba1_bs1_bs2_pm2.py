05f0499aad6dd2a7ddc75659e0696f282a35fd82
gromdimon <grom.dima.grom@gmail.com>
2024-06-24 16:34:28 +0200

    wip

--- src/criteria/auto_ba1_bs1_bs2_pm2.py
+++ src/criteria/auto_ba1_bs1_bs2_pm2.py
@@ -1,13 +1,13 @@
 """Implementation of BA1, BS1, BS2, PM2 prediction for sequence variants."""
 
 import re
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 
 from loguru import logger
 
 from src.api.annonars import AnnonarsClient
 from src.core.config import Config
-from src.defs.annonars_variant import AnnonarsVariantResponse, VariantResult
+from src.defs.annonars_variant import AlleleCount, AnnonarsVariantResponse, VariantResult
 from src.defs.auto_acmg import BA1BS1BS2PM2
 from src.defs.exceptions import AlgorithmError, AutoAcmgBaseException, MissingDataError
 from src.defs.genome_builds import GenomeRelease
@@ -39,137 +39,95 @@ class AutoBA1BS1BS2PM2:
         #: Comment to store the prediction explanation.
         self.comment: str = ""
 
-    def _get_variant_info(self, seqvar: SeqVar) -> Optional[AnnonarsVariantResponse]:
-        """Get variant information from Annonars.
+    def _get_control_af(self, variant_data: VariantResult) -> AlleleCount:
+        """
+        Get the allele frequency for the control population.
+
+        Args:
+            variant_data: The variant data.
 
         Returns:
-            dict: Annonars response.
+            The allele frequency for the control population.
         """
-        try:
-            logger.debug("Getting variant information for {}.", seqvar)
-            return self.annonars_client.get_variant_info(seqvar)
-        except AutoAcmgBaseException as e:
-            logger.error("Failed to get variant information. Error: {}", e)
-            return None
-
-    def evaluate_ba1(self, seqvar: SeqVar, variant_data: VariantResult) -> bool:
-        if (
-            not variant_data.gnomad_exomes
-            or not variant_data.gnomad_exomes.alleleCounts
-            or not variant_data.gnomad_exomes.alleleCounts[0].raw
-            or not variant_data.gnomad_exomes.alleleCounts[0].raw.af
-        ):
+        if not variant_data.gnomad_exomes or not variant_data.gnomad_exomes.alleleCounts:
             raise MissingDataError("No allele counts found in variant data")
+        for af in variant_data.gnomad_exomes.alleleCounts:
+            if af.cohort == "controls":
+                return af
+        raise MissingDataError("No controls allele counts found in variant data.")
 
-        chrom = seqvar.chrom
-        allele_freq = variant_data.gnomad_exomes.alleleCounts[0].raw.af
+    def _get_af(self, seqvar: SeqVar, variant_data: VariantResult) -> float:
+        """
+        Get the allele frequency for the sequence variant.
 
-        if chrom.startswith("chrM"):
-            return allele_freq > 0.01
+        Args:
+            seqvar: The sequence variant.
+            variant_data: The variant data.
+
+        Returns:
+            The allele frequency.
+        """
+        if seqvar.chrom.startswith("M"):
+            if not variant_data.gnomad_mtdna or not variant_data.gnomad_mtdna.afHet:
+                raise MissingDataError("No gnomad data found for mitochondrial variant.")
+            else:
+                return variant_data.gnomad_mtdna.afHet
         else:
-            return allele_freq > 0.05
+            controls_af = self._get_control_af(variant_data)
+            if not controls_af.raw or not controls_af.raw.af:
+                raise MissingDataError("No allele frequency found in control data.")
+            return controls_af.raw.af
 
-    def evaluate_bs1(self, seqvar: SeqVar, variant_data: VariantResult, max_credible_freq) -> bool:
-        if (
-            not variant_data.gnomad_exomes
-            or not variant_data.gnomad_exomes.alleleCounts
-            or not variant_data.gnomad_exomes.alleleCounts[0].byAncestryGroup
-            or not variant_data.gnomad_exomes.alleleCounts[0].raw
-            or not variant_data.gnomad_exomes.alleleCounts[0].raw.af
-        ):
-            raise MissingDataError("No allele counts found in variant data")
-        chrom = seqvar.chrom
-        faf = 0.0
-        for group in variant_data.gnomad_exomes.alleleCounts[0].byAncestryGroup:
-            if group.faf95 and group.faf95 > faf:
-                faf = group.faf95
+    def _check_zygosity(self, variant_data: VariantResult) -> bool:
+        """
+        Check the zygosity of the sequence variant.
 
-        if chrom.startswith("chrM"):
-            return variant_data.gnomad_exomes.alleleCounts[0].raw.af > 0.005
-        else:
-            return faf > max_credible_freq
+        Args:
+            variant_data: The variant data.
 
-    def evaluate_bs2(self, variant_data: VariantResult) -> bool:
-        if (
-            not variant_data.gnomad_exomes
-            or not variant_data.gnomad_exomes.alleleCounts
-            or not variant_data.gnomad_exomes.alleleCounts[0].raw
-            or not variant_data.gnomad_exomes.alleleCounts[0].raw.ac
-            or not variant_data.gnomad_exomes.alleleCounts[0].raw.nhomalt
-        ):
-            raise MissingDataError("No allele counts found in variant data")
-
-        gene_mode = "inplace"  # Field to identify recessive/dominant/X-linked
-        allele_count = variant_data.gnomad_exomes.alleleCounts[0].raw.ac
-
-        if gene_mode in ["recessive", "X-linked"]:
-            return allele_count > 2
-        elif gene_mode == "dominant":
-            return allele_count > 5
+        Returns:
+            True if the variant is recessive (homozygous), dominant (heterozygous), or X-linked
+            (hemizygous) disorder.
+        """
+        controls_af = self._get_control_af(variant_data)
+        if not controls_af.raw or not controls_af.raw.nhomalt:
+            raise MissingDataError("No raw data found in control data.")
+        if controls_af.raw.nhomalt:
+            return True
         return False
-
-    def evaluate_pm2(self, seqvar: SeqVar, variant_data: VariantResult) -> bool:
-        if (
-            not variant_data.gnomad_exomes
-            or not variant_data.gnomad_exomes.alleleCounts
-            or not variant_data.gnomad_exomes.alleleCounts[0].raw
-            or not variant_data.gnomad_exomes.alleleCounts[0].raw.af
-            or not variant_data.gnomad_exomes.alleleCounts[0].raw.ac
-            or not variant_data.gnomad_exomes.alleleCounts[0].raw.nhomalt
-        ):
-            raise MissingDataError("No allele counts found in variant data")
-
-        chrom = seqvar.chrom
-        allele_freq = variant_data.gnomad_exomes.alleleCounts[0].raw.af
-        gene_mode = "inplace"  # Field to identify recessive/dominant/X-linked
-
-        if chrom.startswith("chrM"):
-            return allele_freq < 0.00002
-        else:
-            if gene_mode in ["recessive", "X-linked"]:
-                allele_count = variant_data.gnomad_exomes.alleleCounts[0].raw.ac
-                if allele_count <= 4:
-                    return True
-            elif gene_mode == "dominant":
-                homozygous_count = variant_data.gnomad_exomes.alleleCounts[0].raw.nhomalt
-                if homozygous_count <= 1:
-                    return True
-            return allele_freq < 0.0001
 
     def predict(self) -> Tuple[Optional[BA1BS1BS2PM2], str]:
         """
         Predicts the BA1, BS1, BS2, PM2 criteria for the sequence variant.
 
-
         Note:
             Rules:
-            BA1: Allele frequency is >5% in Exome Sequencing Project, 1000 Genomes Project, or Exome
-            Aggregation Consortium.
+            BA1: Allele frequency is >5%.
 
-            BS1: Allele frequency greater than expected for disorder.
+            BS1: Allele frequency is between 1% and 5%.
 
             BS2: Observed in a healthy adult individual for a recessive (homozygous), dominant
             (heterozygous), or X-linked (hemizygous) disorder, with full penetrance expected at an
             early age.
 
-
-            PM2: Absent from controls (or at extremely low frequency if recessive) in Exome
-            Sequencing Project, 1000 Genomes or ExAC.
+            PM2: Absent from controls allele frequency data.
 
         Returns:
             BA1BS1BS2PM2: The prediction result.
         """
         try:
-            # Instantiate the prediction result
             self.prediction = BA1BS1BS2PM2()
 
-            # Evaluate each criterion
-            self.prediction.BA1 = self.evaluate_ba1(self.seqvar, self.variant_info)
-            self.prediction.BS1 = self.evaluate_bs1(
-                self.seqvar, self.variant_info, max_credible_freq=0.01
-            )  # Adjust max_credible_freq as needed
-            self.prediction.BS2 = self.evaluate_bs2(self.variant_info)
-            self.prediction.PM2 = self.evaluate_pm2(self.seqvar, self.variant_info)
+            af = self._get_af(self.seqvar, self.variant_info)
+            if af > 0.05:
+                self.prediction.BA1 = True
+            elif af >= 0.01:
+                self.prediction.BS1 = True
+            else:
+                self.prediction.PM2 = True
+            if self._check_zygosity(self.variant_info):
+                self.prediction.BS2 = True
+
         except AutoAcmgBaseException as e:
             logger.error("Error occurred during BA1, BS1, BS2, PM2 prediction. Error: {}", e)
             self.comment += f"An error occurred while predicting BA1, BS1, BS2, PM2 criteria: {e}"
