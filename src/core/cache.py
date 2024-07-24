dcdc75edcf307e0af502268968a852b0235736a8
gromdimon <grom.dima.grom@gmail.com>
2024-07-24 16:37:40 +0200

    gitignore

--- src/core/cache.py
+++ src/core/cache.py
@@ -11,16 +11,22 @@ from src.core.config import settings
 class Cache:
     """Cache class to store the results of the Annonars API calls."""
 
-    #: Cache directory
-    CACHE_DIR = os.path.join(settings.PATH_TO_ROOT, "src", "cache")
+    def __init__(self):
+        """Set up the cache directory and settings."""
+        self.use_cache = settings.USE_CACHE
+        self.cache_dir = settings.CACHE_DIR
+        if not os.path.exists(self.cache_dir):
+            os.makedirs(self.cache_dir)
 
     def _get_cache_filename(self, url: str) -> str:
         """Generate a cache filename based on the MD5 hash of the URL."""
         url_hash = hashlib.md5(url.encode()).hexdigest()
-        return os.path.join(self.CACHE_DIR, f"{url_hash}.json")
+        return os.path.join(self.cache_dir, f"{url_hash}.json")
 
     def get(self, url: str) -> Optional[dict]:
         """Check if a cached response exists and return it."""
+        if not self.use_cache:
+            return None
         cache_filename = self._get_cache_filename(url)
         if os.path.exists(cache_filename):
             logger.debug("Loading cached response from: {}", cache_filename)
@@ -30,6 +36,8 @@ class Cache:
 
     def add(self, url: str, response_data: dict) -> None:
         """Cache the response data."""
+        if not self.use_cache:
+            return
         cache_filename = self._get_cache_filename(url)
         logger.debug("Caching response to: {}", cache_filename)
         with open(cache_filename, "w") as cache_file:
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 """pytest configuration file"""
 
+import os
+
 import pytest
 
 from src.core.config import Config
@@ -13,3 +15,13 @@ def api_base_url() -> str:
 @pytest.fixture
 def config(api_base_url: str) -> Config:
     return Config(api_base_url=api_base_url)
+
+
+@pytest.fixture(autouse=True)
+def set_env_vars():
+    """Set and unset environment variables for tests."""
+    # Disable cache for tests
+    os.environ["USE_CACHE"] = "0"
+    yield
+    # Unset environment variables after tests
+    os.environ.pop("USE_CACHE", None)
