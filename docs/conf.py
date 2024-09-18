71ca284f6b61084cd68406916083f4861eda31fe
gromdimon <grom.dima.grom@gmail.com>
2024-09-18 13:25:59 +0200

    rename auto-acmg to AutoACMG in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "auto-acmg"
+project = "AutoACMG"
 copyright = "2024, Dzmitry Hramyka"
 author = "Dzmitry Hramyka"
 release = "0.3.0"
